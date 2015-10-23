da2b551423e9b2fad66d80052ec6edb23bbccf1f
Duncan Mortimer <dmorti@gmail.com>
2015-10-23 15:00:13 +0100

    Fixing indentation

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,9 @@
 ;; Set up package management in init --- allows installing org if missing
 (setq package-archives
       '(("gnu"       . "http://elpa.gnu.org/packages/")
-		("org"       . "http://orgmode.org/elpa/")
-		("melpa"     . "http://melpa.milkbox.net/packages/")
-		("marmalade" . "http://marmalade-repo.org/packages/")))
+	("org"       . "http://orgmode.org/elpa/")
+	("melpa"     . "http://melpa.milkbox.net/packages/")
+	("marmalade" . "http://marmalade-repo.org/packages/")))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
